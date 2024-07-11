2e3ab5dd872e25b89506b2db3bb15a7cf1a170fd
Oyatulloh <izzatullayevoyatullo08@gmail.com>
2024-07-11 08:23:21 +0500

    s

---
+++ home.js
@@ -0,0 +1,32 @@
+function button1(){
+    onclick=document.getElementById('header-text1').innerHTML="Dunyo Saytlari";alert("Sahifa o'zbek tiliga o'girilsinmi ?");
+    onclick=document.getElementById('header-text2').innerHTML="Assalomu Aleykum bu bizning dunyo malumotlarni malum bir miqdorda o'ziga saqlagan saytimizga xush kelibsiz! Bu saytda siz turli xil malumotlar bilan tanishishingiz mumkin! Men sizga bu sayt haqida biroz malumot berib o'taman! Bu saytda siz Mashinalar, Turli hil odamlar ,Taniqli taomlar Elektronika(Kompyuterlar)bilan va yana G'aroyib narsalar bilan tanishib ular haqida malumotlar yig'ishingiz mumkin! Bu malumotlarni saytimizning kegingi qismlaridan topasiz. Sizni saytimizda ko'rib tuganimizdan hursandmiz Tashrifingiz uchun raxmat !"
+    onclick=document.getElementById('header-button3').innerHTML="Ru";
+    onclick=document.getElementById('header-button2').innerHTML="En";
+    onclick=document.getElementById('header-button1').innerHTML="Uz";
+    onclick=document.getElementById('header-button4').innerHTML="TJ";
+}
+function button2(){
+    onclick=document.getElementById('header-text1').innerHTML="World Sites";alert("Can you change the site to English?");
+    onclick=document.getElementById('header-text2').innerHTML="Hello, welcome to our site, which has a certain amount of world information! on this site you can get acquainted with various information! I will give you some information about this site! On this site, you can learn about Cars, Different People, Famous Foods, Electronics (Computers) and other Strange Things! You will find this information in the next parts of our site! We are glad to see you on our site. Thank you for your visit!";
+    onclick=document.getElementById('header-button3').innerHTML="Ru";
+    onclick=document.getElementById('header-button2').innerHTML="En";
+    onclick=document.getElementById('header-button1').innerHTML="Uz";
+    onclick=document.getElementById('header-button4').innerHTML="TJ";
+}
+function button3(){
+    onclick=document.getElementById('header-text1').innerHTML="Мировые сайты";alert("Будете ли вы менять сайт на русский?");
+    onclick=document.getElementById('header-text2').innerHTML="Здравствуйте, добро пожаловать на наш сайт, на котором собрано определенное количество мировой информации! На этом сайте вы можете ознакомиться с различной информацией! Я дам вам некоторую информацию об этом сайте! На этом сайте вы можете узнать об автомобилях, разных людях, известных продуктах питания, электронике (компьютерах) и других странных вещах! Эту информацию вы найдете в следующих разделах нашего сайта! Мы рады видеть Вас на нашем сайте. Спасибо за Ваш визит!"
+    onclick=document.getElementById('header-button3').innerHTML="РУ";
+    onclick=document.getElementById('header-button2').innerHTML="ЕН";
+    onclick=document.getElementById('header-button1').innerHTML="УЗ";
+    onclick=document.getElementById('header-button4').innerHTML="ТЖ";
+}
+function button4(){
+    onclick=document.getElementById("header-text1").innerHTML="Сайтҳои ҷаҳонӣ";alert("Шумо сайтро ба забони тоҷикӣ иваз мекунед?");
+    onclick=document.getElementById('header-text2').innerHTML="Салом, хуш омадед ба сайти мо, ки микдори муайяни иттилооти чахониро дорад! Дар ин сайт шумо метавонед бо маълумоти гуногун шинос шавед! Ман ба шумо дар бораи ин сайт маълумот медиҳам! Дар ин сайт шумо метавонед дар бораи мошинҳо, одамони гуногун, хӯрокҳои машҳур, электроника (компютерҳо) ва дигар чизҳои аҷиб маълумот гиред! Шумо ин маълумотро дар қисматҳои ояндаи сайти мо хоҳед ёфт! Мо аз дидани шумо дар сайти мо шодем, барои боздидатон ташаккур!";
+    onclick=document.getElementById('header-button3').innerHTML="РУ";
+    onclick=document.getElementById('header-button2').innerHTML="ЕН";
+    onclick=document.getElementById('header-button1').innerHTML="УЗ";
+    onclick=document.getElementById('header-button4').innerHTML="ТЖ";
+}
